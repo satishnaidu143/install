5041bf5043349b33d12bd984025c5d580a521dcb
CHANDHU naidu <mnsvp11@gmail.com>
2019-11-27 16:28:30 +0530

    updated

---
+++ tomcat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update 
+sudo apt-get install openjdk-8-jdk -y
+wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
+tar -xvf apache-tomcat-8.5.49.tar.gz
+cd apache-tomcat-8.5.49/bin
+sh startup.sh
+echo
+echo 
+echo "edit context.xml & tomcat-users.xml & service.xml and restart tomcat again"
