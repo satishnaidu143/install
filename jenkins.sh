d85b693f35b3cc6e447996665cb956e2e277e6d8
CHANDHU naidu <mnsvp11@gmail.com>
2019-11-27 15:15:44 +0530

    installation scriptings

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk -y
+sudo apt-get install maven -y
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+  
---
+++ kube.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
