bc8f523fc5aeac5056610e9e13d6506b70e509bd
CHANDHU naidu <mnsvp11@gmail.com>
2019-12-11 15:39:59 +0530

    jenkins file updates

--- jenkins.sh
+++ jenkins.sh
@@ -6,5 +6,9 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
+sleep 10s
+echo "find the jenkins password"
+echo
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
-  
+echo
+echo
