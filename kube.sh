26e523855845c021f7942763245c57a301de5f41
satish <mnsvp111@gmail.com>
2020-02-07 18:32:06 +0530

    ansable playbookss

--- kube.sh
+++ kube.sh
@@ -10,5 +10,3 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
-
