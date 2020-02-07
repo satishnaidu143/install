9822f2f1240f7d819ada5ecf9dd493607a33cef0
satish <mnsvp111@gmail.com>
2020-02-07 18:10:48 +0530

    ansable playbooks

--- kube.sh
+++ kube.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+sudo usermod -aG docker ansable
 apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
@@ -9,4 +10,5 @@ EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
--- tomcat.sh
+++ tomcat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get install openjdk-8-jdk -y
-wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.49.tar.gz
-tar -xvf apache-tomcat-8.5.49.tar.gz
-cd apache-tomcat-8.5.49/bin
+wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.50.tar.gz
+tar -xvf apache-tomcat-8.5.50.tar.gz
+cd apache-tomcat-8.5.50/bin
 sh startup.sh
 echo
 echo 
