4c9bc87e54de57adbc0c61aa6fee30d58601fef9
satish <mnsvp111@gmail.com>
2020-02-12 13:16:17 +0530

    downloadurl updated

--- tomcat.sh
+++ tomcat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get install openjdk-8-jdk -y
-wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.49/bin/apache-tomcat-8.5.50.tar.gz
+wget http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.50/bin/apache-tomcat-8.5.50.tar.gz
 tar -xvf apache-tomcat-8.5.50.tar.gz
 cd apache-tomcat-8.5.50/bin
 sh startup.sh
