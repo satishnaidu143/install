b3659edc11fb7d938cb94e7e43e491d0a501718d
satish <mnsvp11@gmail.com>
2020-05-07 12:35:44 +0530

    updated

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
 sudo apt-get install maven -y
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo sh -c 'deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
 sleep 10s
--- tomcat.sh
+++ tomcat.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get install openjdk-8-jdk -y
-wget http://apachemirror.wuchna.com/tomcat/tomcat-8/v8.5.51/bin/apache-tomcat-8.5.51.tar.gz
-tar -xvf apache-tomcat-8.5.51.tar.gz
-cd apache-tomcat-8.5.51/bin
-sh startup.sh
+wget http://us.mirrors.quenda.co/apache/tomcat/tomcat-8/v8.5.54/bin/apache-tomcat-8.5.54.tar.gz
+tar -xvf apache-tomcat-8.5.54.tar.gz
+sh apache-tomcat-8.5.54/bin/startup.sh
 echo
 echo 
-echo "edit context.xml==> vi apache-tomcat-8.5.51/webapps/host-manager/META-INF/contex.xml
+echo "edit context.xml==> vi apache-tomcat-8.5.54/webapps/host-manager/META-INF/contex.xml
 .*
 
-tomcat-users.xml==> vi apache-tomcat-8.5.51/conf/tomcat-users.xml
+tomcat-users.xml==> vi apache-tomcat-8.5.54/conf/tomcat-users.xml
 
 <?xml version='1.0' encoding='utf-8'?>
 <tomcat-users>
