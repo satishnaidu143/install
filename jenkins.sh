ce6fa9c75b9b4b305dd4a140f2f23a6b9c61b6ef
satish <mnsvp11@gmail.com>
2020-05-23 14:24:53 +0530

    updated

--- jenkins.sh
+++ jenkins.sh
@@ -2,8 +2,9 @@
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk -y
 sudo apt-get install maven -y
-wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list'
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
+    /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins -y
 sleep 10s
