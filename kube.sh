c7b609f02b9fb0316ca5f7c8d92d42e036a0a686
satishnaidu143 <54499707+satishnaidu143@users.noreply.github.com>
2020-06-30 14:06:43 +0530

    Update kube.sh
    testing my luck

--- kube.sh
+++ kube.sh
@@ -1,12 +1,3 @@
-#!/bin/bash
-#curl -fsSL https://get.docker.com -o get-docker.sh
-#sh get-docker.sh
-#usermod -aG docker ubuntu jenkins
-#apt-get update && apt-get install -y apt-transport-https curl
-#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-#cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-#deb https://apt.kubernetes.io/ kubernetes-xenial main
-#EOF
 apt-get update && apt-get install -y \
   apt-transport-https ca-certificates curl software-properties-common gnupg2
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -29,14 +20,13 @@ cat > /etc/docker/daemon.json <<EOF
 }
 EOF
 mkdir -p /etc/systemd/system/docker.service.d
-# Restart Docker
-systemctl daemon-reload
-systemctl restart docker
-sudo systemctl enable docker
+apt-get update && apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF      
+EOF
 apt-get update
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
+systemctl restart docker
+kubeadm init
