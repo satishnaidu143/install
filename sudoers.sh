da9158e741712b5bdfcbadbeb7e37dfee41d90a7
satish <mnsvp11@gmail.com>
2020-06-30 11:52:45 +0530

    kube file updated with cgroups as a sustemd

--- sudoers.sh
+++ sudoers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 while [[ -n $1 ]]; do
-    echo "$1    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
+    echo "$1    ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers;
     shift # shift all parameters;
 done
