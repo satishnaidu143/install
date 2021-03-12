27ae1bde2d4791e507b274ee71e606684bbce554
satishnaidu143 <54499707+satishnaidu143@users.noreply.github.com>
2021-03-13 01:01:13 +0530

    update wave net command

--- kube_network.sh
+++ kube_network.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
