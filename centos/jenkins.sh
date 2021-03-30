f22b4bf52c43b9602f6d3699424e62b4921afd7e
satishnaidu143 <54499707+satishnaidu143@users.noreply.github.com>
2021-03-30 16:01:11 +0530

    jenkins

--- centos/jenkins.sh
+++ centos/jenkins.sh
@@ -2,7 +2,7 @@
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
-curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
+sudo curl --silent --location http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo | sudo tee /etc/yum.repos.d/jenkins.repo
 sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 sudo yum install jenkins -y
 sudo systemctl start jenkins
